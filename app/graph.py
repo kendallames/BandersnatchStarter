dac34c79306d1f9ddd65a4167c1ac29afa5e93da
kjwhines <kendallamesss@gmail.com>
2023-05-02 12:20:08 -0700

    Updated second ticket

--- app/graph.py
+++ app/graph.py
@@ -1,5 +1,48 @@
-from altair import Chart
+from altair import Chart, Tooltip
+from pandas import DataFrame
 
 
-def chart(df, x, y, target) -> Chart:
-    pass
+def chart(df: DataFrame, x: str, y: str, target: str) -> Chart:
+    """ Creates a chart that displays the x, y and target from the Dataframe. Modified configurations of the graph to
+    make the graph visually appealing."""
+
+    font = 'Calibre'
+    graph = Chart(
+        data=df,
+        background="#222222",
+        title=f"{y} by {x} for {target}",
+    ).mark_circle(
+        size=100
+    ).encode(
+        x=x,
+        y=y,
+        color=target,
+        tooltip=Tooltip(df.columns.to_list())
+    ).properties(
+        width=500,
+        height=500,
+        padding=32
+    ).configure_title(
+        fontSize=30,
+        font=font,
+        color='white',
+    ).configure_legend(
+        labelFont=font,
+        titleFont=font,
+        titleColor='white',
+        labelColor='white',
+        labelFontSize=12,
+        titlePadding=15,
+        titleFontSize=15
+    ).configure_axis(
+        labelFont=font,
+        titleFont=font,
+        titleColor='white',
+        labelColor='white',
+        titleFontSize=20,
+        labelFontSize=12,
+        titlePadding=15
+    )
+
+    return graph
+
